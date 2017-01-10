24b93b257e6444bd2f8fbd9ece6c34e341fc9b01
Arnold Lei <arnold.lei88@gmail.com>
2017-01-09 21:54:01 -0500

    working on the topic controller

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,6 +1,10 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller
+    alias Discuss.Topic
+
     def new(conn, params) do
-        
+        struct = %Topic{}
+        params = %{}
+        changeset = Topic.changeset(struct, params)
     end
 end
